e6dc978f999e6c7dee7508261d1c096e4bf976b4
seba17962 <isebsebastiansilva@gmail.com>
2023-12-20 23:17:31 +0000

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import requests
 from sys import argv
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """_summary_
     """
     employee_ID = argv[1]
@@ -20,8 +20,6 @@ if __name__ == '__main__':
     response_user = requests.get(url_user)
     count = 0
 
-
-
     if response_todos.status_code >= 400 and response_user.status_code >= 400:
         print("Error fetching data")
         exit()
@@ -29,15 +27,15 @@ if __name__ == '__main__':
     response_todos_json = response_todos.json()
     response_user_json = response_user.json()
 
-    name = response_user_json["name"]
+    name = response_user_json['name']
 
     for task in response_todos_json:
-        if task["completed"] is True:
+        if task['completed'] is True:
             count += 1
 
     print(f"Employee {name} is\
  done with tasks({count}/{len(response_todos_json)}):")
 
     for task in response_todos_json:
-        if task["completed"] is True:
+        if task['completed'] is True:
             print(f"\t {task['title']}")
